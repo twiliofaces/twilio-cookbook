511a20a355b7938d45b9e29c9882237bf2350ee7
fiorenzo pizza <fiorenzino@gmail.com>
2013-10-29 01:05:16 +0100

    recipe3 first commit

--- chapter1/recipe3/src/main/java/org/twiliofaces/chapter1/recipe3/controller/OrderController.java
+++ chapter1/recipe3/src/main/java/org/twiliofaces/chapter1/recipe3/controller/OrderController.java
@@ -14,38 +14,84 @@ import javax.enterprise.context.RequestScoped;
 import javax.inject.Inject;
 import javax.inject.Named;
 
+import org.twiliofaces.inject.notification.AccountSid;
+import org.twiliofaces.inject.notification.Body;
 import org.twiliofaces.inject.notification.From;
+import org.twiliofaces.inject.notification.MessageSid;
+import org.twiliofaces.inject.notification.To;
 
 @Named
 @RequestScoped
-public class OrderController {
+public class OrderController
+{
 
-	Logger logger = Logger.getLogger(OrderController.class.getName());
+   Logger logger = Logger.getLogger(OrderController.class.getName());
 
-	private Map<Long, String> orders;
-	
-	@Inject
-	@From
-	String from;
-	
+   private Map<Long, String> orders;
 
+   @Inject
+   @Body
+   String body;
 
-	public void reply() {
+   @Inject
+   @MessageSid
+   String messageSid;
 
-	}
+   @Inject
+   @AccountSid
+   String accountSid;
 
-	public Map<Long, String> getOrders() {
-		if (orders == null) {
-			orders = new HashMap<Long, String>();
-			orders.put(111L, "shipped");
-			orders.put(222L, "processing");
-			orders.put(333L, "awaiting fullfillment");
-		}
-		return orders;
-	}
+   @Inject
+   @From
+   String from;
 
-	public void setOrders(Map<Long, String> orders) {
-		this.orders = orders;
-	}
+   @Inject
+   @To
+   String to;
+
+   public String reply()
+   {
+      if (body != null && !body.isEmpty())
+      {
+         if (getOrders().containsKey(body.trim()))
+         {
+            return "Your order is currently set at status '" + getOrders().get(body.trim()) + "'";
+         }
+         else
+         {
+            return "No Order Matching that ID was found";
+         }
+      }
+      else
+      {
+         return "Please send us your order id and we will look it up ASAP";
+      }
+   }
+
+   public void evaluate()
+   {
+      logger.info("messageSid: " + messageSid);
+      logger.info("accountSid: " + accountSid);
+      logger.info("body: " + body);
+      logger.info("from: " + from);
+      logger.info("to: " + to);
+   }
+
+   public Map<Long, String> getOrders()
+   {
+      if (orders == null)
+      {
+         orders = new HashMap<Long, String>();
+         orders.put(111L, "shipped");
+         orders.put(222L, "processing");
+         orders.put(333L, "awaiting fullfillment");
+      }
+      return orders;
+   }
+
+   public void setOrders(Map<Long, String> orders)
+   {
+      this.orders = orders;
+   }
 
 }
