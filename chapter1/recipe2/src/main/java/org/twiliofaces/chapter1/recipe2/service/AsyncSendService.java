f29474f53d08b8f920cccc6093b5dc66a03611f0
fiorenzo pizza <fiorenzino@gmail.com>
2013-11-02 01:47:20 +0100

    correct sms.send() => simpleSend()

--- chapter1/recipe2/src/main/java/org/twiliofaces/chapter1/recipe2/service/AsyncSendService.java
+++ chapter1/recipe2/src/main/java/org/twiliofaces/chapter1/recipe2/service/AsyncSendService.java
@@ -13,22 +13,27 @@ import com.twilio.sdk.TwilioRestException;
 
 @Stateless
 @LocalBean
-public class AsyncSendService {
+public class AsyncSendService
+{
 
-	static String ENDPOINT = "";
-	@Inject
-	Sender sender;
+   static String ENDPOINT = "";
+   @Inject
+   Sender sender;
 
-	Logger logger = Logger.getLogger(AsyncSendService.class.getName());
+   Logger logger = Logger.getLogger(AsyncSendService.class.getName());
 
-	@Asynchronous
-	public void sendPassword(String number, String body) {
-		try {
-			String sid = sender.to(number).body(body).send();
-			logger.info("msg id:" + sid);
-		} catch (TwilioRestException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-	}
+   @Asynchronous
+   public void sendPassword(String number, String body)
+   {
+      try
+      {
+         String sid = sender.to(number).body(body).simpleSend();
+         logger.info("msg id:" + sid);
+      }
+      catch (TwilioRestException e)
+      {
+         // TODO Auto-generated catch block
+         e.printStackTrace();
+      }
+   }
 }
