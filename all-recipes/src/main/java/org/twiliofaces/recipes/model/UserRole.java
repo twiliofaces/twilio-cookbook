623478b8bf6aba88d0c91e7773a94cdb7880b851
fiorenzo <fiorenzino@gmail.com>
2013-12-30 00:36:35 +0100

    add license to all classes

--- all-recipes/src/main/java/org/twiliofaces/recipes/model/UserRole.java
+++ all-recipes/src/main/java/org/twiliofaces/recipes/model/UserRole.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2013 GiavaCms.org.
+ * Copyright 2013 twiliofaces.org.
  *
  * Licensed under the Eclipse Public License version 1.0, available at
  * http://www.eclipse.org/legal/epl-v10.html
